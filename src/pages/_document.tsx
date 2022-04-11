725c4e5ffe8b42bc6e52694f4dca7eedcc77a548
Jean Martins <jeanmrtns4@gmail.com>
2022-04-11 12:34:34 -0300

    feat(contact page): added script to link with mailchimp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,9 @@ export default class myDocument extends Document {
         <body>
           <NextScript />
           <Main />
+          <script id="mcjs" dangerouslySetInnerHTML={{ __html: `
+          !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/9fd3aba42c3b9153c9a48a234/16623333376276411a81def03.js");` }} />
+          <script ></script>
         </body>
       </Html>
     )
